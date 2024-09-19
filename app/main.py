a8a455d41c11c3871f092ad20084752ea2f6d2fb
l-pim <lukas.pimenta137@gmail.com>
2024-09-19 16:38:05 +0200

    commit jeudi docker verif

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
-from fastapi import FastAPI
-import json, os
-from pydantic import BaseModel
-from typing import List, Optional, Dict, Any
+from fastapi.testclient import TestClient
+from .main import app
 
-# Initialize FastAPI app
-app = FastAPI()
+client = TestClient(app)
 
-@app.get("/")
-async def test():
-    return {"message": "OK"}
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "OK"}
